fda866f4bca2f4c7a49061e64f346a81f126f340
Felipe Sepulvida <felipe.sepulvida@injunior.com.br>
2023-04-08 12:26:54 -0300

    feat: finished right page interface

--- src/components/RightPage/index.jsx
+++ src/components/RightPage/index.jsx
@@ -1,10 +1,44 @@
 import React from "react";
-import { PageBackground, PageBorder } from "./styles";
+import { BottomScreensContainer, MainButton, MainButtonsContainer, MainScreen, PageBackground, PageBorder, Searchbar, SearchContainer } from "./styles";
+import { SmallButton } from "../SmallButton";
+
+import infoIcon from '../../assets/icons/user.svg'
+import descIcon from '../../assets/icons/message.svg'
+import statIcon from '../../assets/icons/dumbell.svg'
+import evolIcon from '../../assets/icons/pokeball.svg'
+import searchIcon from '../../assets/icons/search.svg'
+import { SmallScreen } from "../SmallScreen";
 
 export function RightPage(){
     return(
         <PageBorder>
             <PageBackground>
+                <MainScreen />
+                <MainButtonsContainer>
+                    <MainButton>
+                        <img src={infoIcon} alt='' />
+                    </MainButton>
+                    <MainButton>
+                        <img src={descIcon} alt='' />
+                    </MainButton>
+                    <MainButton>
+                        <img src={statIcon} alt='' />
+                    </MainButton>
+                    <MainButton>
+                        <img src={evolIcon} alt='' />
+                    </MainButton>
+                </MainButtonsContainer>
+                <SearchContainer>
+                    <Searchbar> 
+                        <img src={searchIcon} alt='' />
+                        <input />
+                    </Searchbar>
+                    <SmallButton />
+                </SearchContainer>
+                <BottomScreensContainer>
+                    <SmallScreen />
+                    <SmallScreen />
+                </BottomScreensContainer>
                 
             </PageBackground>
         </PageBorder>
--- src/components/RightPage/styles.js
+++ src/components/RightPage/styles.js
@@ -24,7 +24,126 @@ export const PageBackground = styled.div`
 
     width: 421px;
     height: 665px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 25px;
     
     -webkit-clip-path: polygon(44% 16.7%, 100% 16.7%, 100% 100%, 0 100%, 0 0, 19% 0);
     clip-path: polygon(44% 16.7%, 100% 16.7%, 100% 100%, 0 100%, 0 0, 19% 0);
+`
+
+const Container = styled.div`
+    box-sizing: border-box;
+
+    img{
+        width: 24px;
+        height: 24px;
+    }
+`
+
+export const MainScreen = styled(Container)`
+    background-color: #3CC850;
+
+    width: 300px;
+    height: 168px;
+
+    margin-top: 150px;
+
+    border: 5px solid black;
+    border-radius: 10px;
+
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+
+export const MainButtonsContainer = styled(Container)`
+    background-color: black;
+
+    width: 300px;
+    height: 54px;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    border-radius: 10px;
+
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    > * {
+        &:first-child{
+            border-radius: 5px 0px 0px 5px;
+        }
+        &:last-child{
+            border-radius: 0px 5px 5px 0px;
+        }
+    }
+`
+
+export const MainButton = styled(Container)`
+    background-color: #28B4FA;
+ 
+    width: 68.75px;
+    height: 44px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+`
+
+export const SearchContainer = styled(Container)`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 10px;
+`
+
+export const Searchbar = styled(Container)`
+    background-color: #D9D9D9;
+
+    width: 300px;
+    height: 44px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 5px;
+
+    padding: 5px;
+
+    border: 5px solid black;
+    border-radius: 10px;
+
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    input{
+        background-color: transparent;
+
+        font-family: 'Roboto', sans-serif;
+
+        height: 100%;
+
+        flex: 1;
+
+        border: none;
+
+        &:focus{
+            outline: none;
+        }
+    }
+`
+
+export const BottomScreensContainer = styled(Container)`
+    width: 300px;
+    
+    flex: 1;
+
+    margin-bottom: 30px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `
